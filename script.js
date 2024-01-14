a030aefc02f30657b5fccb86bbc2fc3b3192606d
rishiyadav54 <rishi54yadav@gmail.com>
2024-01-14 23:51:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+gsap.from("#footer h1",{
+    scale:5,
+    scrollTrigger:{
+        trigger:"#footer",
+        scoller:"body",
+        start:"top 10%",
+        scrub:2,
+        pin:true,
+    }
+})
